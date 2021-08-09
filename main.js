20a304fdce2bccd5e862d57fa86ef616b06df5ea
wownsl456 <wownsl456@naver.com>
2021-08-09 18:37:05 +0900

    Update

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ document.addEventListener('scroll', () => {
     }
 }); 
 
+
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
